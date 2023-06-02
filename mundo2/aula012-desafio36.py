5dca406cbcce7ba93224052d1af86d3e9f763df4
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2023-06-02 07:33:01 -0300

    update aula12

---
+++ mundo2/aula012-desafio36.py
@@ -0,0 +1,5 @@
+#Escreva um programa para aprovar o emprestimo bancário para a compra de uma casa.
+#O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+#Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário,
+#caso contrário, o emprestimo será negado.
+
