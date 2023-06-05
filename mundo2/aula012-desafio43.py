dfb4fc286563cf9c26001d8a785c42a7282c3420
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2023-06-05 14:14:53 -0300

    update 43

--- mundo2/aula012-desafio43.py
+++ mundo2/aula012-desafio43.py
@@ -5,3 +5,22 @@
 #De 25.1 até 30: SOBREPESO;
 #De 30.1 até 40: OBESIDADE;
 #Acima de 40.1: OBESIDADE MÓRBIDA.
+
+print('Lets calculate your Body mass index (BMI)!')
+w = float(input('How much do you weigh (Kg)?'))
+h = float(input('What is your height? (m)'))
+
+bmi = w/(h**2)
+
+if bmi <= 18.5:
+    print(f'Your BMI is about {bmi:.1f}, and you are UNDER WEIGHT.')
+elif bmi <= 25:
+    print(f'Your BMI is about {bmi:.1f}, and you have the IDEAL WEIGHT.')
+elif bmi <= 30:
+    print(f'Your BMI is about {bmi:.1f}, and you are OVERWEIGHT.')
+elif bmi <= 40:
+    print(f'Your BMI is about {bmi:.1f}, and you have OBESITY.')
+else:
+    print(f'Your BMI is about {bmi:.1f}, and you have MORBID OBESITY.')
+
+
