d065759bc50aafc4fa5155aa334a43f7b72ae7b8
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2023-06-05 06:06:31 -0300

    update

---
+++ mundo2/aula012-desafio43.py
@@ -0,0 +1,7 @@
+#Desenvolva uma lógica que leia o peso e altura de uma pessoa. Calcule seu IMC e mostre
+#seu status, de acordo com a tabela abaixo:
+#Abaixo de 18.5: ABAIXO DO PESO;
+#Entre 18.5 e 25: PESO IDEAL;
+#De 25.1 até 30: SOBREPESO;
+#De 30.1 até 40: OBESIDADE;
+#Acima de 40.1: OBESIDADE MÓRBIDA.
---
+++ mundo2/aula012-desafio45.py
@@ -0,0 +1,2 @@
+#Crie um programa que faça o computador jogar JOKENPÔ com você.
+#(pedra/papel/tesoura) - sorteio/random
