a9a8dab5698fdd5a1088a3698cb976a212c6fe90
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2023-06-05 11:56:48 -0300

    update 42

--- mundo2/aula012-desafio42.py
+++ mundo2/aula012-desafio42.py
@@ -1,4 +1,23 @@
-#Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
-#Equiláteri: todos os lados iguais;
+#Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo
+#será formado:
+#Equilátero: todos os lados iguais;
 #Isósceles: dois lados iguais;
 #Escaleno: todos os lados diferentes.
+
+ladoA = float(input('Digite o primeiro segmento:'))
+ladoB = float(input('Digite o segundo segmento:'))
+ladoC = float(input('Digite o terceiro segmento:'))
+
+if ladoA < ladoB + ladoC and ladoB < ladoA + ladoC and ladoC < ladoA + ladoB:
+    print('Triangulo POSSÍVEL')
+
+    if ladoA == ladoB == ladoC:
+        print('As medidas formam um triângulo EQUILÁTERO.')
+    elif ladoA != ladoB != ladoC != ladoA:
+        print('As medidas formam um triângulo ESCALENO.')
+    else:                   #ou ladoA == ladoB != ladoC or ladoB == ladoC != ladoA or ladoC == ladoA != ladoB:
+        print('As medidas formam um triângulo ISÓSCELES.')
+
+else:
+    print('Triangulo IMPOSSÍVEL, tente novamente')
+
