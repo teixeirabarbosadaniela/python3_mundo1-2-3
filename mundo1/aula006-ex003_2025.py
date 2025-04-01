f66ec792ffb2051abffc7630c7a12cb73707975c
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-04-01 09:37:57 -0300

    atualizações

---
+++ mundo1/aula006-ex003_2025.py
@@ -0,0 +1,11 @@
+#desafio extra
+#minha tentativa
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1+n2
+#print('A soma entre ', n1, ' e ', n2, ' vale ', s, '.')
+print('A soma entre {0} e {1} vale {2}.'.format(n1, n2, s))
+
+#outros métodos
+#n = input('Digite algo: ')
+#print(n.is______)
